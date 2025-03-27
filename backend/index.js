cf2fb94feba37c8bdcf1e40f54bff974d8d42c55
Raju <rajutamang3456@gmail.com>
2025-03-27 15:53:05 +0545

    schema

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,29 @@
 const express = require("express");
 const app = express();
+const dbConnect = require("./db");
 const dotenv = require("dotenv");
 const { chats } = require("./data/data");
+
 dotenv.config();
 const port = process.env.PORT;
 console.log(port);
+dbConnect();
 
-app.get("/", (req, res) => {
-  res.send("Hello my friend! how are you? sanchoi hununcha??");
-});
-app.get("/chats", (req, res) => {
-  res.send(chats);
-});
-app.get("/chats/:id", (req, res) => {
-  // console.log(req);
-  // res.send(req.params);
-  const singleChat = chats.find((chat) => chat._id === req.params.id);
-  res.send(singleChat);
-  // console.log(singleChat);
-});
+// app.get("/", (req, res) => {
+//   res.send("api is listening");
+// });
+// app.get("/chats", (req, res) => {
+//   res.send(chats);
+// });
+// app.get("/chats/:id", (req, res) => {
+//   // console.log(req);
+//   // res.send(req.params);
+//   const singleChat = chats.find((chat) => chat._id === req.params.id);
+//   res.send(singleChat);
+//   // console.log(singleChat);
+// });
+
+app.use("/api/auth", require("./routes/Auth"));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
