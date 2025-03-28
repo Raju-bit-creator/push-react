a4c9099150b01460942303dc36d81028d4baa6aa
Raju <rajutamang3456@gmail.com>
2025-03-28 15:48:46 +0545

    create user

--- backend/index.js
+++ backend/index.js
@@ -4,24 +4,26 @@ const dbConnect = require("./db");
 const dotenv = require("dotenv");
 const { chats } = require("./data/data");
 
+app.use(express.json());
+
 dotenv.config();
 const port = process.env.PORT;
 console.log(port);
 dbConnect();
 
-// app.get("/", (req, res) => {
-//   res.send("api is listening");
-// });
-// app.get("/chats", (req, res) => {
-//   res.send(chats);
-// });
-// app.get("/chats/:id", (req, res) => {
-//   // console.log(req);
-//   // res.send(req.params);
-//   const singleChat = chats.find((chat) => chat._id === req.params.id);
-//   res.send(singleChat);
-//   // console.log(singleChat);
-// });
+app.get("/", (req, res) => {
+  res.send("api is listening");
+});
+app.get("/chats", (req, res) => {
+  res.send(chats);
+});
+app.get("/chats/:id", (req, res) => {
+  // console.log(req);
+  // res.send(req.params);
+  const singleChat = chats.find((chat) => chat._id === req.params.id);
+  res.send(singleChat);
+  // console.log(singleChat);
+});
 
 app.use("/api/auth", require("./routes/Auth"));
 
--- backend/routes/Auth.js
+++ backend/routes/Auth.js
@@ -1,2 +1,24 @@
 const express = require("express");
-const app = express();
+const User = require("../model/User");
+const router = express.Router();
+
+// register user
+router.post("/createuser", async (req, res) => {
+  try {
+    let user = await User.findOne({ email: req.body.email });
+    if (user) return res.status(400).send({ message: "Email already exists" });
+
+    user = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    res.json({ user });
+  } catch (error) {
+    res.status(500).send("internal server error");
+  }
+});
+
+// login
+//user detail
+module.exports = router;
